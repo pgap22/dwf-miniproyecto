20b4d2189f34e7cc99ec84a40607ad90e9b282c8
Gerardo Saz <gerardo.saz120@gmail.com>
2025-09-01 18:38:26 -0600

    chore(init): inicializar proyecto Spring Boot 3.5.5 con Maven

---
+++ src/test/java/sv/edu/udb/data_collector/DataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.data_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
