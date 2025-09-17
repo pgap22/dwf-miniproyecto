46e6268343a52063de7f23550466d6d92e381306
Gerardo Saz <gerardo.saz120@gmail.com>
2025-09-16 23:24:02 -0600

    fix service

--- src/main/java/sv/edu/udb/data_collector/service/DataTypeService.java
+++ src/main/java/sv/edu/udb/data_collector/service/DataTypeService.java
@@ -1,33 +1,15 @@
 package sv.edu.udb.data_collector.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import sv.edu.udb.data_collector.domain.DataType;
-import sv.edu.udb.data_collector.repository.DataTypeRepository;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class DataTypeService {
 
-    private final DataTypeRepository repository;
+public interface DataTypeService {
 
-    /**
-     * Primitivos = todos excepto 'CATALOG'
-     */
-    public List<DataType> listPrimitives() {
-        return repository.findAllByNameNotOrderByNameAsc("CATALOG");
-    }
+    public List<DataType> listPrimitives();
 
-    /**
-     * (Opcional) todos, por si luego lo necesitas
-     */
-    public List<DataType> listAll() {
-        return repository.findAllByOrderByNameAsc();
-    }
+    public List<DataType> listAll();
 
-    public DataType getById(String id) {
-        return repository.findById(id).orElse(null);
-    }
+    public DataType getById(String id);
 }
