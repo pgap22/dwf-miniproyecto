f24e3035d8ef52a0e3b514b74f4a070b784cf687
Gerardo Saz <gerardo.saz120@gmail.com>
2025-09-18 18:45:03 -0600

    refactor(validation-rules): remove CRUD operations and request class, update seeding script

--- src/main/java/sv/edu/udb/data_collector/service/ValidationRuleService.java
+++ src/main/java/sv/edu/udb/data_collector/service/ValidationRuleService.java
@@ -2,13 +2,9 @@ package sv.edu.udb.data_collector.service;
 
 import java.util.List;
 
-import sv.edu.udb.data_collector.controller.request.ValidationRuleRequest;
 import sv.edu.udb.data_collector.domain.ValidationRule;
 
 public interface ValidationRuleService {
     List<ValidationRule> findAll();
     ValidationRule findById(String id);
-    ValidationRule create(ValidationRuleRequest request);
-    ValidationRule update(String id, ValidationRuleRequest request);
-    void delete(String id);
 }
